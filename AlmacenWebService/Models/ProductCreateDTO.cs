f168b98ccdbba4a9f61ba16fbdda8a55f4ec0700
Albertraza <arbert.jr@hotmail.com>
2020-09-27 20:04:20 -0400

    add products dtos

---
+++ AlmacenWebService/Models/ProductCreateDTO.cs
@@ -0,0 +1,24 @@
+﻿using AlmacenWebService.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlmacenWebService.Models
+{
+    public class ProductCreateDTO
+    {
+        [Required]
+        [FirstLetterShouldBeUpperCaseAtribute]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        [Required]
+        [Range(0.00, 99999999999999999)]
+        public double Price { get; set; }
+        [Required]
+        [GreaterThanZeroAtribute]
+        public int Quantity { get; set; }
+    }
+}
---
+++ AlmacenWebService/Models/ProductDTO.cs
@@ -0,0 +1,24 @@
+﻿using AlmacenWebService.Utilities;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace AlmacenWebService.Models
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [FirstLetterShouldBeUpperCaseAtribute]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        [Required]
+        [Range(0.00, 99999999999999999)]
+        public double Price { get; set; }
+        [Required]
+        [GreaterThanZeroAtribute]
+        public int Quantity { get; set; }
+    }
+}
